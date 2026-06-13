5054b4568ac56e698032505569cc4eb678fecffe
uchoamarcos <uchoamarcos307@gmail.com>
2026-06-13 11:49:15 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function Sufjanstevens() {
-  const spanResposta = document.querySelector("#resposta");
-  let mensagem;
-  mensagem = "SUFJAN STEVENS"
-  spanResposta.innerHTML = `<b>${mensagem}</b>`;
-}
