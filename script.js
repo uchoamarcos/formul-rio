02b30b15bfc2393d8d05ee4ff0c5bdc81adff9b5
uchoamarcos <uchoamarcos307@gmail.com>
2026-06-13 11:45:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function Sufjanstevens() {
+  const spanResposta = document.querySelector("#resposta");
+  let mensagem;
+  mensagem = "SUFJAN STEVENS"
+  spanResposta.innerHTML = `<b>${mensagem}</b>`;
+}
